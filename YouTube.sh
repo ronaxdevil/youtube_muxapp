97611e94b6ce0194c7c93eabf9686ab508c65007
kotapioneers <ronaxb0@gmail.com>
2026-01-24 13:28:44 +0530

    Adapt for muos. use proper portmaster lib to make it work.

--- YouTube.sh
+++ YouTube.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
-
 # YouTube App - PortMaster Launch Script
+
+# --- 1. PortMaster Detection ---
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
   controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
 else
   controlfolder="/roms/ports/PortMaster"
 fi
 
 source $controlfolder/control.txt
+get_controls
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
+# --- 2. Directory Setup ---
+GAMEDIR="/$directory/MUOS/Applications/Youtube/youtube"
+
+# --- 3. Move into the directory ---
+cd "$GAMEDIR"
+
+# --- 4. Exports & Library Setup ---
 export LD_LIBRARY_PATH="$controlfolder/libs:$controlfolder/utils/lib:$LD_LIBRARY_PATH"
 export PYTHONPATH="$controlfolder/exlibs:$controlfolder/pylibs:$controlfolder/libs:$PYTHONPATH"
 export PYSDL2_DLL_PATH="$controlfolder/libs"
+export PATH="$GAMEDIR:$PATH"
 
-# yt-dlp için PATH ekle (pip install ile kurulan)
-export PATH="$HOME/.local/bin:/home/ark/.local/bin:$PATH"
-
-# YouTube App dizini
-GAMEDIR="/roms/ports/youtube"
-
-cd $GAMEDIR
-
-# Terminal ayarları
+# Terminal settings
 $ESUDO chmod 666 /dev/tty0
 export TERM=linux
 printf "\033c" > /dev/tty0
@@ -32,5 +38,5 @@ printf "\033c" > /dev/tty0
 # YouTube uygulamasını başlat
 $ESUDO python3 youtube.py > "$GAMEDIR/log.txt" 2>&1
 
-# Temizlik
+# Cleanup
 printf "\033c" > /dev/tty0
